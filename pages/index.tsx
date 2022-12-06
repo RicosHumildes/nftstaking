07c552c38c9fd42956584c1fe5ab72068369da1e
RicosHumildes <ricoshumildes@gmail.com>
2022-12-06 22:30:29 +0100

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,44 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      {/* Top Section */}
+      <h1 className={styles.h1}>Haz staking de tus NFTs INVESTOR y consigue recompensas</h1>
+      <div className={styles.nftBoxGrid}>
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/mint`)}
+        >
+          {/* Mint a new NFT */}
+            <img src={`/icons/drop.webp`} alt="drop" />
+            <h2 className={styles.selectBoxTitle}>Crea un nuevo NFT</h2>
+            <p className={styles.selectBoxDescription}>
+              Usa este butón para conseguir un nuevo NFT.
+            </p>
+        </div>
+
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/stake`)}
+        >
+          {/* Staking an NFT */}
+          <img src={`/icons/token.webp`} alt="drop" />
+          <h2 className={styles.selectBoxTitle}>Stakea tus NFTs</h2>
+          <p className={styles.selectBoxDescription}>
+            Use the custom staking contract deployed via <b>thirdweb Deploy</b>{" "}
+            to stake your NFTs, and earn tokens from the <b>Token</b> contract.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
